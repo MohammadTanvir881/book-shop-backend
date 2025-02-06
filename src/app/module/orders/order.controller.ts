6cb66bc3ccbfcc0bda7bb27d3c5e68841be7013c
MohammadTanvir881 <tanvirrashid881@gmail.com>
2025-02-05 21:04:21 -1100

    link fixed for payment gateway

--- src/app/module/orders/order.controller.ts
+++ src/app/module/orders/order.controller.ts
@@ -34,8 +34,8 @@ const createOrder = async (req: Request, res: Response) => {
       total_amount: order.totalPrice,
       currency: "BDT",
       tran_id: tran_id, // use unique tran_id for each api call
-      success_url: `http://localhost:5000/payment/success/${tran_id}`,
-      fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+      success_url: `https://book-shop-as.vercel.app/payment/payment/success/${tran_id}`,
+      fail_url: `https://book-shop-as.vercel.app/payment/payment/fail/${tran_id}`,
       cancel_url: "http://localhost:3030/cancel",
       ipn_url: "http://localhost:3030/ipn",
       shipping_method: "Courier",
